53e88749d425f8d16ccc57848818fc6fa139ecd9
thawzin.dev <thawzin99777@gmail.com>
2022-10-07 12:18:07 +0630

    logstash update, store log in local if env is local

--- app/Logging/LogstashLogger.php
+++ app/Logging/LogstashLogger.php
@@ -4,11 +4,17 @@ namespace App\Logging;
 use Monolog\Logger;
 
 class LogstashLogger {
-
+    
     public function __invoke(array $config)
     {
-        
         $logger = new Logger($config['channel']);
-        return $logger->pushHandler(new LogstashHandler(Logger::DEBUG,true,$config['with']));
+        
+        $server_info =  [
+            'address' => config('logging.address'),
+            'port' => config('logging.port')
+        ];
+        
+        return $logger->pushHandler(new LogstashHandler($server_info, Logger::DEBUG, true));
+        
     }
 }
